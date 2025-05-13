c908b18f887e67f707821ec8bef4a851127e317f
Usman <mohammad.abbas@alasala.edu.sa>
2025-05-13 10:47:58 +0300

    3rd test after logout from browser

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","usman","khalid"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","usman","khalid","farhan"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
